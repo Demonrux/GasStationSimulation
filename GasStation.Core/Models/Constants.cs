dd98d4dfffc8275c934d45092fb7942dd36ae8e1
Dmitriy <kirilllukanov121@gmail.com>
2025-11-21 21:20:00 +0300

    Update Constants.cs

--- GasStation.Core/Models/Constants.cs
+++ GasStation.Core/Models/Constants.cs
@@ -7,8 +7,8 @@
         public const double FuelCoefficient = 0.1;
         public const double BaseRefuelingTime = 3.0;
         public const double BasePaymentTime = 2.0;
-        public const int MinCarFuelAmount = 10;
-        public const int MaxCarFuelAmount = 40;
+        public const int MinCarFuelAmount = 5;
+        public const int MaxCarFuelAmount = 80;
         public const int FuelTruckAmount = 400;
         public const int FuelThreshold = 100;
     }
